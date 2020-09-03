d8e38e4ed26c4b213da584b9d159c34f6892e2d2
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2020-09-03 17:21:22 +0300

    add template

---
+++ src/js/app.js
@@ -0,0 +1,52 @@
+/* eslint-disable linebreak-style */
+// TODO: write your code here
+import Character from './character';
+
+class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(name) {
+    const test1 = new Character();
+    let step = 0;
+    this.members = test1.profiles.reduce((result, profile) => {
+      try {
+        if (step === 1) throw new Error('Существуют два аналогичных персонажа');
+        if (profile.name === name) {
+          result.add(profile.name);
+          step += 1;
+        }
+      } catch (e) {
+        console.error(e);
+      }
+      return result;
+    }, new Set());
+  }
+
+  addAll(...rest) {
+    const test1 = new Character();
+    for (let i = 0; i < rest.length; i += 1) {
+      this.members = test1.profiles.reduce((result, profile) => {
+        if (profile.name === rest[i]) {
+          result.add(profile.name);
+        }
+        return result;
+      }, new Set());
+    }
+  }
+}
+
+
+const test1 = new Team();
+// test1.add('Bowman');
+console.log(test1.members);
+test1.addAll('Bowman', 'Bowman', 'Magican', 'Swordman');
+console.log(test1.members);
+
+// console.log(test1);
+// Character {profiles: Array(2)}
+// profiles: (2) [{…}, {…}]
+// __proto__: Object
+// console.log(test1.profiles);
+// [{…}, {…}]0: {name: "Bowman"}1: {name: "Swordman"}length: 2__proto__: Array(0)
---
+++ src/js/character.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+class Character {
+  constructor() {
+    this.profiles = [
+      { name: 'Bowman' },
+      { name: 'Swordman' },
+      { name: 'Swordman' },
+      { name: 'Magican' },
+    ];
+  }
+}
+
+export default Character;
